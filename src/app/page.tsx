409fab922e7e8167c9ae1e6d5476de88d19c24ac
SaifAlisoomro <hk4948171@gmail.com>
2024-11-24 23:28:00 +0500

    todo app with next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [todos, settodos] = useState([
+    { movie: "Titanic", id: 2 },
+    { movie: "The World", id: 2 },
+  ]);
+
+  const [inputval, setInput] = useState("");
+  const [Id, setId] = useState(0);
+
+  const additems = () => {
+    let object = todos.find((item) => item.id == Id);
+    if (object) {
+      let newArray = todos.filter((item) => item.id !== object.id);
+      settodos([...newArray, { movie: inputval, id: Id }]);
+      setInput("");
+      setId(0);
+      return;
+    }
+    settodos([...todos, { movie: inputval, id: Id }]);
+    setInput("");
+    setId(0);
+  };
+
+  const Edititem = (id: number) => {
+    let object = todos.find((item) => item.id === id);
+    if (object) {
+      setInput(object.movie);
+      setId(object.id);
+    }
+  };
+
+  const deleteitem = (id: number) => {
+    let newArray = todos.filter((item) => item.id !== id);
+    settodos([...newArray]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="max-w-4xl mx-auto p-5 shadow-md shadow-black rounded-sm font-sans mt-10 lg:mt-32">
+      <h1 className="text-center text-3xl lg:text-[40px] font-sans font-bold pb-5 text-gray-600">
+        TODO APP
+      </h1>
+      <div className="flex flex-col lg:flex-row justify-around gap-5">
+        <input
+          className="w-full lg:w-[60%] p-2 text-lg shadow focus:outline-none"
+          placeholder="Add Movies"
+          type="text"
+          value={inputval}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
         />
+        <input
+          className="w-full lg:w-[20%] p-2 text-lg shadow focus:outline-none"
+          placeholder="Add I'd"
+          type="number"
+          value={Id}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setId(Number(e.target.value))
+          }
+        />
+        <button
+          onClick={additems}
+          className="w-full lg:w-[20%] bg-blue-800 text-white font-sans font-bold rounded-md text-center py-2 hover:bg-slate-700"
+        >
+          Add Movie
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h1 className="text-center text-3xl lg:text-[40px] font-sans font-bold pb-5 mt-5 text-gray-600">
+        Movies List
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {todos.map((item, i) => (
+          <div key={i} className="shadow p-4 bg-white rounded-md">
+            <div className="flex justify-between items-center">
+              <span className="shadow rounded-full w-8 h-8 text-center">
+                {i + 1}
+              </span>
+              <span
+                onClick={() => deleteitem(item.id)}
+                className="shadow rounded-full w-8 h-8 text-center cursor-pointer text-red-800"
+              >
+                X
+              </span>
+            </div>
+            <div className="mt-5 text-lg lg:text-[30px] text-gray-600">
+              {item.movie}
+            </div>
+            <div>
+              <h2
+                onClick={() => Edititem(item.id)}
+                className="text-right font-bold cursor-pointer text-blue-700"
+              >
+                Edit
+              </h2>
+            </div>
+          </div>
+        ))}
       </div>
-    </main>
+    </div>
   );
 }
